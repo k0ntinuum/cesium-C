bf1adff045e6b287079020bffcff031423de75de
k0ntinuum <k0ntinuum@proton.me>
2023-11-15 07:42:20 -0500

    sign impr

--- cesium.c
+++ cesium.c
@@ -1,28 +1,27 @@
 #include<stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
-//typedef int64_t i64;
+//#include <unistd.h>
+#include <inttypes.h>
 typedef u_int32_t u32;
-const u32 N = 10;
-const u32 B = 10;
-const u32 T = 27;
-u32 F[N][N] = {0};
-u32 f[N][N] = {0};
-u32 g[N][N] = {0};
-u32 temp[N] = {};
-u32 p[T];
-u32 c[T];
-u32 d[T];
-u32 check_p[T];
-u32 e[T];
-u32 w[T];
-
+typedef u_int64_t u64;
+const u32 N = 16;
+const u32 T = 32;
+const u32 B = 27;
+//char alph[] = "O|2";
+char alph[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ_";
+#include"shuffle.c"
+#include"randomize.c"
+#include"key.c"
 #include"print.c"
-#include"matrix.c"
+//#include"matrix.c"
 #include"encode.c"
-#include"encrypt.c"
+//#include"encrypt.c"
+#include"encryption_demo.c"
 #include"demo.c"
 	
 int main() {
-	encrypt_demo();
+	encoding_demo();
+	//demo();
+
 }
--- print.c
+++ print.c
@@ -4,64 +4,56 @@ void rgb(int r, int g, int b) {
 void cls() {
     printf( "\x1b[2J");
 }
+void reverse_text() {
+    printf("\x1b[7m");
+}
+void stop_reverse_text() {
+    printf("\x1b[27m");
+}
+void hide_cursor() {
+    printf("\x1b[?25l");
+}
+void show_cursor() {
+    printf("\x1b[?25h");
+}
 
 void cursor_to(int r, int c) {
     printf("\x1b[%d;%dH",r,c);
 }
 
+void print_symbol(u32 i) {
+    printf("%c", alph[i]);
+}
+    // switch (i) {
+    //     case 0 : printf("O");break;
+    //     case 1 : printf("|");break;
+    //     case 2 : printf("2");break;
+    //     default : printf("\u2591");break;
+    // }
+
 // void print_symbol(u32 i) {
 //     switch (i) {
 //         case 0 : printf("O");break;
-//         case 1 : printf("|");break;
-//         case 2 : printf("@");break;
-//         case 3 : printf("*");break;
-//         case 0 : printf("O");break;
-//         case 1 : printf("|");break;
-//         case 2 : printf("@");break;
-//         case 3 : printf("*");break;
-//         case 0 : printf("O");break;
-//         case 1 : printf("|");break;
-//         case 2 : printf("@");break;
-//         case 3 : printf("*");break;
+//         case 1 : printf("\u2588");break;
+//         case 2 : printf("|");break;
+//         default : printf("\u2591");break;
 //     }
 // }
-void print_symbol(u32 i) {
-    switch (i) {
-        case 0 : printf("O");break;
-        case 1 : printf("|");break;
-        case 2 : printf("2");break;
-        case 3 : printf("3");break;
-        case 4 : printf("4");break;
-        case 5 : printf("5");break;
-        case 6 : printf("6");break;
-        case 7 : printf("7");break;
-        case 8 : printf("8");break;
-        case 9 : printf("9");break;
-        case 10 : printf("A");break;
-        case 11 : printf("B");break;
-        case 12 : printf("C");break;
-        case 13 : printf("D");break;
-        case 14 : printf("E");break;
-        case 15 : printf("F");break;
-    }
-}
-
-void print_f(u32 h[N][N]) {
+void print_key(const u32 k[N][N]) {
 	for (u32 i = 0; i < N; i++) {
-		 for (u32 j = 0; j < N; j++) {
-            print_symbol( h[i][j] );
-            printf(" ");
-         }
-		 printf("\n");
+		for (u32 j = 0; j < N; j++) { print_symbol(k[i][j]); printf(" ");}
+        //"\u2588\u2588"); //"\u2591\u2591"
+		printf("\n");
 	}
-}
-void print_text(u32 t[N]) {
-	for (u32 j = 0; j < T; j++) print_symbol( t[j] );
+    printf("\n");
 }
 
-void print_status() {
-        printf("p = ");print_text(p);printf("\n");
-        printf("c = ");print_text(c);printf("\n");
-        printf("d = ");print_text(d);printf("\n\n");
+void print_vec(u32* v, u32 l) {
+	for (u32 j = 0; j < l; j++) printf("%d ", v[j]);
+    printf("\n");
 }
+void print_text(u32 t[T]) {
+	for (u32 i = 0; i < T; i++) print_symbol(t[i]);
+}
+
 	
